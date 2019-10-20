8b4f0ab19063217d00419bb0c2ee57d89a31a9e0
Alex Ionescu <alxi.2001@gmail.com>
2019-10-20 22:15:56 +0300

    Benchmark compile & execution time

--- src/Bfi/Program.fs
+++ src/Bfi/Program.fs
@@ -1,13 +1,22 @@
 module Bfi.Program
 
+open System.Diagnostics
 open System.IO
 
 [<EntryPoint>]
 let main argv =
-  argv.[0]
-  |> File.ReadAllText
-  |> Parser.parse
-  |> Optimizer.optimize
-  |> Codegen.run
+  let sw = Stopwatch.StartNew()
 
+  let ast =
+    argv.[0]
+    |> File.ReadAllText
+    |> Parser.parse
+    |> Optimizer.optimize
+
+  printfn "[Compile: %dms]" sw.ElapsedMilliseconds
+
+  sw.Restart()
+  Codegen.run ast
+
+  printfn "\n[Run: %dms]" sw.ElapsedMilliseconds
   0
